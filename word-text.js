6b552c7f91f46cbea7ab5326ed95fd12ecb55370
qaboxletstest <qaboxletstest@gmail.com>
2021-06-18 19:20:17 +0530

    Extract text from MS Word

---
+++ word-text.js
@@ -0,0 +1,33 @@
+const WordExtractor = require("word-extractor");
+// const extractor = new WordExtractor();
+// const extracted = extractor.extract("word-text.docx");
+const fs = require("fs")
+
+// extracted.then(docx => {
+//     // console.log(docx.getBody())
+//     // console.log(docx.getHeaders())
+//     // console.log(docx.getFootnotes())
+//     console.log(docx.getTextboxes())
+// }).catch(err => {
+//     console.log(err.message)
+// })
+
+const getWordText = async (fileName) => {
+    try {
+        if (fs.existsSync(fileName)) {
+            const extractor = new WordExtractor()
+            const extracted = await extractor.extract(fileName)
+            if (extracted) {
+                return extracted.getBody()
+            }
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+getWordText("word-text.docx").then(content => {
+    console.log(content)
+}).catch(errMsg => {
+    console.log(errMsg)
+})
